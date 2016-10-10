4af762d705cbab590767e6819069fae5d407eb13
Copperfiled <guan.dassy@gmail.com>
2016-10-10 18:39:13 +0800

    修改Swift版本的Podfile

--- templates/swift/Example/PROJECT/ViewController.swift
+++ templates/swift/Example/PROJECT/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+    }
 }
 
